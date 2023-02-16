b8f5767618d8f4ccd8781cb944a3d8ca8c72b7f0
Wole Ojo <woleojopb@gmail.com>
2023-02-16 13:31:09 +0100

    1st milestone in lesson2

--- index.js
+++ index.js
@@ -1,69 +1,70 @@
-
-const computerChoiceDisplay = document.getElementById("computer-choice")
-const userChoiceDisplay = document.getElementById("your-choice")
-const reslutDisplay= document.getElementById("result")
-const possibleChoices = document.querySelectorAll("button")
-
-let userChoice;
-
-
-possibleChoices.forEach(button=>{
-    button.addEventListener("click", (e)=>{
-        
-        userChoice = e.target.id
-        userChoiceDisplay.innerHTML = userChoice;
-        generateResult();
-        generateComputerChoice();
-       
-    })
-})
-
-
-let computerChoice;
-
-let generateComputerChoice = () =>{
-    let num = Math.floor(Math.random() * 3);
-    let computerChoices = ["rock", "paper", "scissors"]
+const cardArry = [
+    {
+        name:"fries",
+        img:"images/fries.png"
+    },
+    {
+        name:"cheeseburger",
+        img:"images/cheeseburger.png"
+    },
+    {
+        name:"hotdog",
+        img: 'images/hotdog.png'
+    },
+    {
+        name:"ice-cream",
+        img: 'images/ice-cream.png'
+    },
+    {
+        name:"milkshake",
+        img: 'images/milkshake.png'
+    },
+    {
+        name:"pizza",
+        img: 'images/pizza.png'
+    },
+    {
+        name:"fries",
+        img:"images/fries.png"
+    },
+    {
+        name:"cheeseburger",
+        img:"images/cheeseburger.png"
+    },
+    {
+        name:"hotdog",
+        img: 'images/hotdog.png'
+    },
+    {
+        name:"ice-cream",
+        img: 'images/ice-cream.png'
+    },
+    {
+        name:"milkshake",
+        img: 'images/milkshake.png'
+    },
+    {
+        name:"pizza",
+        img: 'images/pizza.png'
+    },
     
-    computerChoice = computerChoices[num];
-    computerChoiceDisplay.innerHTML = computerChoice
+]
 
+cardArry.sort(()=>0.5-Math.random())
+
+
+const displayGrid = document.querySelector("#grid")
+
+
+function createBoard(){
+    for (i=0; i<cardArry.length; i++){
+        const card = document.createElement("img")
+        card.setAttribute("src","images/blank.png")
+        card.setAttribute("data-id",i)
+        console.log(card);
+        displayGrid.append(card)
+    }
 }
 
-let result;
-function generateResult() {
 
-    if(userChoice === computerChoice){
-        result = "it's a draw"
-    }
-    if (userChoice==="paper" && computerChoice ==="rock"){
-        result = "You win!"
-    }
-    if (userChoice==="paper" && computerChoice ==="scissors"){
-        result = "You lose!"
-    }
-    if (userChoice==="scissors" && computerChoice ==="paper"){
-        result = "You win!"
-    }
-    if (userChoice==="scissors" && computerChoice ==="rock"){
-        result = "You lose!"
-    }
-    if (userChoice==="rock" && computerChoice ==="paper"){
-        result = "You lose!"
-    }
-    if (userChoice==="rock" && computerChoice === "scissors"){
-        result = "You win!"
-    }
-
-    reslutDisplay.innerHTML = result
-
-}
-
-
-
-    
-        
-
-
-
-
+createBoard();
